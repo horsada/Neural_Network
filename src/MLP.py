6ae1fc98672c6c7b09540afd0cbb176c49d7297c
horsada <ah2719@imperial.ac.uk>
2020-12-25 15:50:57 +0000

    Chapter 2 Done

--- src/MLP.py
+++ src/MLP.py
@@ -8,7 +8,36 @@ class Perceptron:
 
     def __init__(self, inputs, bias = 1.0):
         """Return a new Perceptron object with the specified number of inputs (+1 for the bias).""" 
+        self.weights = (np.random.rand(inputs+1) * 2) - 1
+        self.bias = bias
 
     def run(self, x):
         """Run the perceptron. x is a python list with the input values."""
-        
+        total = np.dot(np.append(x,self.bias), self.weights)
+        return self.sigmoid(total)
+
+    def set_weights(self, w_init):
+        self.weights = np.array(w_init, float)
+       # assert len(w_init) == self.inputs
+
+    def sigmoid(self,x):
+        return 1/(1+np.exp(-x))
+
+# AND Gate Test
+"""
+neuron = Perceptron(2)
+neuron.set_weights([10,10,-15])
+
+print(neuron.run([1,1]))
+"""
+
+# OR Gate Test
+"""
+neuron = Perceptron(2)
+neuron.set_weights([10,10,-5])
+
+print(neuron.run([0,0]))
+print(neuron.run([0,1]))
+print(neuron.run([1,0]))
+print(neuron.run([1,1]))
+"""
