cbf69faf870a151aca496a22e5c180e3cec72778
horsada <ah2719@imperial.ac.uk>
2020-12-26 12:08:20 +0000

    Multilayer Perceptron

--- Notes.md
+++ Notes.md
@@ -54,5 +54,20 @@ Consists of 3 layers:
 2. Hidden (composed of neurons)
 3. Output 
 
-### The Brain
+## Building blocks of NN's
 
+Brain neurons:
+- Dendrite => Take electrical signals 
+- Nucleus => Processing of input occurs for an output
+- Axon terminal => Output path to the desired location
+
+Simple neuron model:
+- Weighting of x_0 to x_n-1 inputs 
+- Summing these inputs
+- Need a bias term independent of inputs (x_n) to shift the line (i.e y intercept)
+- Activation Function => Provide nonlinearity, constrain output value and model desired threshold behaviour
+
+Activation functions:
+- Binary step => u(x); 0 if x<0, 1 if x>=0.
+- Logistic/Sigmoid => 1/(1+e^-x); all values between 0 and 1. 
+- Hyperbolic tangent => tanh(x); all values between -1 and 1.  
--- src/MLP.py
+++ src/MLP.py
@@ -40,7 +40,32 @@ class MultiLayerPerceptron:
         self.network = [] # The list of lists of neurons
         self.values = []  # The list of lists of output values        
         
-
+        for i in range(len(self.layers)):
+            self.network.append([])
+            self.values.append([])
+            self.values[i] = [0.0 for j in range(len(self.layers))]
+            if i > 0:
+                for j in range(len(self.layers)):
+                    self.network[i].append(Perceptron(inputs = self.layers[i-1]), bias = self.bias)
         
         self.network = np.array([np.array(x) for x in self.network],dtype=object)
         self.values = np.array([np.array(x) for x in self.values],dtype=object)
+
+    def set_weights(self, w_init):
+        for i in range(len(w_init)):
+            for j in range(len(w_init[i])):
+                self.network[i+1][j].set_weights(w_init[i][j])
+
+    def printWeights(self):
+        print()
+        for i in range(1, len(self.network)):
+            for j in range(self.layers[i]):
+                print("Layer", i+1, "Neuron", j, self.network[i][j].weights)
+
+    def run(self, x):
+        x = np.array(x, dtype = object)
+        self.values[0] = x
+        for i in range(1, len(self.network)):
+            for j in range(self.layers[i]):
+                self.values[i][j] = self.network[i][j].run(self.values[i-1])
+        return self.values[-1]
